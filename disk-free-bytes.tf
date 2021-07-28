4d43d66fa73a3f050b9b55650a63ede8490be4fe
Rik Tonnard <rik.tonnard@me.com>
2021-07-28 16:01:34 +0200

    Add disk free bytes monitor

---
+++ disk-free-bytes.tf
@@ -0,0 +1,33 @@
+locals {
+  disk_free_bytes_filter = coalesce(
+    var.disk_free_bytes_filter_override,
+    var.filter_str
+  )
+}
+
+module "disk_free_bytes" {
+  source = "git@github.com:kabisa/terraform-datadog-generic-monitor.git?ref=0.6.2"
+
+  name             = "System - Disk Free (bytes)"
+  query            = "avg(${var.disk_free_bytes_evaluation_period}):min:system.disk.free{${local.disk_free_bytes_filter}} by {host,device} < ${var.disk_free_bytes_critical}"
+  alert_message    = "Available disk volume {{device.name}} on ${var.service} Node {{host.name}} has dropped below {{threshold}} ( {{device.name}}  has {{value}}% available on {{host.name}} )"
+  recovery_message = "Available disk volume {{device.name}} on ${var.service} Node {{host.name}} has recovered ( {{device.name}}  has {{value}}% available on {{host.name}} )"
+
+  # monitor level vars
+  enabled            = var.disk_free_bytes_enabled
+  alerting_enabled   = var.disk_free_bytes_alerting_enabled
+  warning_threshold  = var.disk_free_bytes_warning
+  critical_threshold = var.disk_free_bytes_critical
+  priority           = var.disk_free_bytes_priority
+  docs               = var.disk_free_bytes_docs
+  note               = var.disk_free_bytes_note
+
+  # module level vars
+  env                  = var.alert_env
+  service              = var.service
+  notification_channel = var.notification_channel
+  additional_tags      = var.additional_tags
+  locked               = var.locked
+  name_prefix          = var.name_prefix
+  name_suffix          = var.name_suffix
+}
--- disk-free-percent.tf
+++ disk-free-percent.tf
@@ -1,26 +1,26 @@
 locals {
-  disk_free_filter = coalesce(
-    var.disk_free_filter_override,
+  disk_free_percent_filter = coalesce(
+    var.disk_free_percent_filter_override,
     var.filter_str
   )
 }
 
-module "disk_free" {
+module "disk_free_percent" {
   source = "git@github.com:kabisa/terraform-datadog-generic-monitor.git?ref=0.6.2"
 
-  name             = "System - Disk Free"
-  query            = "avg(${var.disk_free_evaluation_period}):( min:system.disk.free{${local.disk_free_filter}} by {host,device} / min:system.disk.total{${local.disk_free_filter}} by {host,device} ) * 100 < ${var.disk_free_critical}"
+  name             = "System - Disk Free (percentage)"
+  query            = "avg(${var.disk_free_percent_evaluation_period}):( min:system.disk.free{${local.disk_free_percent_filter}} by {host,device} / min:system.disk.total{${local.disk_free_percent_filter}} by {host,device} ) * 100 < ${var.disk_free_percent_critical}"
   alert_message    = "Available disk volume {{device.name}} on ${var.service} Node {{host.name}} has dropped below {{threshold}} ( {{device.name}}  has {{value}}% available on {{host.name}} )"
   recovery_message = "Available disk volume {{device.name}} on ${var.service} Node {{host.name}} has recovered ( {{device.name}}  has {{value}}% available on {{host.name}} )"
 
   # monitor level vars
-  enabled            = var.disk_free_enabled
-  alerting_enabled   = var.disk_free_alerting_enabled
-  warning_threshold  = var.disk_free_warning
-  critical_threshold = var.disk_free_critical
-  priority           = var.disk_free_priority
-  docs               = var.disk_free_docs
-  note               = var.disk_free_note
+  enabled            = var.disk_free_percent_enabled
+  alerting_enabled   = var.disk_free_percent_alerting_enabled
+  warning_threshold  = var.disk_free_percent_warning
+  critical_threshold = var.disk_free_percent_critical
+  priority           = var.disk_free_percent_priority
+  docs               = var.disk_free_percent_docs
+  note               = var.disk_free_percent_note
 
   # module level vars
   env                  = var.alert_env
