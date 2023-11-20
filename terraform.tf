35155dfe2b58035d50a8f52be6505d28193a25d9
Mathieu <mathieu@weglot.com>
2023-11-20 23:34:38 +0100

    chore: update datadog provider and version

--- terraform.tf
+++ terraform.tf
@@ -2,7 +2,10 @@ terraform {
   required_providers {
     datadog = {
       source  = "DataDog/datadog"
-      version = "~> 3.12"
+      version = "~> 3.32"
     }
   }
+
+  required_version = ">= 1.4.0"
+
 }
